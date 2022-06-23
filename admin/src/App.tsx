488078eda032093ff80fa1830e9f5f7952de2fd4
jordanos <y.dereje@yahoo.com>
2022-06-23 15:46:48 +0300

    chore(admin-setup): admin setup

---
+++ admin/src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+const App = () => {
+  const newLocal = "App";
+  return (
+    <div className={newLocal}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
