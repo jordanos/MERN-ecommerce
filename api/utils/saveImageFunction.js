ee6912bd7a318263b4109e8895c74bda29addfa0
jordanos <y.dereje@yahoo.com>
2022-07-01 11:36:10 +0300

    fix(api-saveImage): save image path edited for heroku

--- api/utils/saveImageFunction.js
+++ api/utils/saveImageFunction.js
@@ -5,7 +5,7 @@ const saveImageFunction = async (req, imagePath) => {
   const filename = Date.now() + path.extname(req.file.originalname);
   req.file.filename = filename;
   // location: where to store resized photo
-  const location = `./public/${imagePath}/${filename}`;
+  const location = `./public${imagePath}${filename}`;
   // toFile() method stores the image on disk
   await sharp(req.file.buffer).resize(300, 300).toFile(location);
 };
